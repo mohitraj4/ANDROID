ef5df194c21f8b1969aa54f23bcdf1e94927be5f
mohitraj4 <80747182+mohitraj4@users.noreply.github.com>
2023-04-17 17:53:53 +0530

    Add files via upload

---
+++ addvideo.java
@@ -0,0 +1,85 @@
+package com.kazimasum.videostreaming;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class addvideo extends AppCompatActivity
+{
+    VideoView videoView;
+    Button browse,upload;
+    Uri videouri;
+    MediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addvideo);
+
+        videoView=(VideoView)findViewById(R.id.videoView);
+        upload=(Button)findViewById(R.id.upload);
+        browse=(Button)findViewById(R.id.browse);
+        mediaController=new MediaController(this);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+                browse.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Dexter.withContext(getApplicationContext())
+                                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                                .withListener(new PermissionListener() {
+                                    @Override
+                                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                        Intent intent=new Intent();
+                                        intent.setType("video/*");
+                                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                                        startActivityForResult(intent,101);
+                                    }
+
+                                    @Override
+                                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                                    }
+
+                                    @Override
+                                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                     permissionToken.continuePermissionRequest();
+                                    }
+                                }).check();
+                    }
+                });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==101 && resultCode==RESULT_OK)
+        {
+            videouri=data.getData();
+            videoView.setVideoURI(videouri);
+
+        }
+
+    }
+}
